852ace94f8e7553464dc46b67e9fd4b4909cf2ac
Udi Finkelstein <github@udifink.com>
2022-11-04 10:25:27 +0200

    Updated sin/cos module.

--- Arcade_template/sin_cos.sv
+++ Arcade_template/sin_cos.sv
@@ -1,12 +1,11 @@
 module sin_cos #(
     parameter ROM_DEPTH=256,  // number of entries in sine ROM for 0° to 90°
-    parameter WIDTH=18,   // width of sine ROM data in bits
-    localparam ADDRW=$clog2(4*ROM_DEPTH)  // full circle is 0° to 360°
+    parameter WIDTH=18   // width of sine ROM data in bits
 ) (
     input clk,
-    input  wire logic [ADDRW-1:0] phase,  // fraction, 0 to 2*PI
-    output      logic signed [WIDTH-1:0] sin_val,  // result
-    output      logic signed [WIDTH-1:0] cos_val  // result
+    input  logic        [$clog2(ROM_DEPTH)+2-1:0] phase,  // fraction, 0 to 2*PI
+    output logic signed [WIDTH-1:0] sin_val,  // result
+    output logic signed [WIDTH-1:0] cos_val  // result
 );
 
     // sine table ROM: 0°-90°
@@ -23,7 +22,7 @@ module sin_cos #(
     logic [1:0] quad;  // quadrant we're in: I, II, III, IV
     always_comb begin
         // 2nd and 4th quadrants use mirrored data
-        rom_addr = quad[0] ? (ROM_DEPTH - 1) - phase[ADDRW-3:0];
+        rom_addr = quad[0] ? (ROM_DEPTH - 1) - phase[$clog2(ROM_DEPTH)-1:0] : phase[$clog2(ROM_DEPTH)-1:0];
         sin_val = (quad == 2 || quad == 3) ? -sin_data : sin_data;
         cos_val = (quad == 1 || quad == 2) ? -cos_data : cos_data;
     end
