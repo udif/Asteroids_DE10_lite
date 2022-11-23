9d23629f7d53610598121dd86ea50259895534e7
Udi Finkelstein <github@udifink.com>
2022-11-23 19:02:28 +0200

    Internal changes in Screen_display to use new vga 'interface' construct.

--- Arcade_template/Screens_dispaly.sv
+++ Arcade_template/Screens_dispaly.sv
@@ -32,23 +32,15 @@ module Screens_dispaly # (
 	wire	[3:0]		Red_i;
 	wire	[3:0]		Green_i;
 	wire	[3:0]		Blue_i;
-	wire				disp_ena;
-	wire	[$clog2(WIDTH )-1:0]Pxl_x_i;
-	wire	[$clog2(HEIGHT)-1:0]Pxl_y_i;
 	
-	wire h_sync_d;
-	wire v_sync_d;
+vga vga_chain_start();
 
 // VGA controller
  vga_controller VGA_interface (
 	.pixel_clk  (clk_25),
-   .reset_n    (1),
-   .h_sync     (h_sync_d),
-   .v_sync     (v_sync_d),
-   .disp_ena   (disp_ena),
-   .column     (Pxl_x_i),
-   .row        (Pxl_y_i)
-   );
+	.reset_n    (1),
+    .vga_gen    (vga_chain_start)
+);
 	
 // LCD controller
 lcd_ctrl LCD_interface(
@@ -56,8 +48,8 @@ lcd_ctrl LCD_interface(
 	.clk_25(clk_25),
 	.clk_100(clk_100),
 	.resetN(1),
-	.pxl_x(Pxl_x_i),
-	.pxl_y(Pxl_y_i),
+	.pxl_x(vga_chain_start.t.pxl_x),
+	.pxl_y(vga_chain_start.t.pxl_y),
 	.h_sync(0), // not used
 	.v_sync(0), // not used
 	.red_in(Red_i),
@@ -73,13 +65,13 @@ lcd_ctrl LCD_interface(
 
 
 // screen out display picker / enable
-assign Red_i = (disp_ena == 1'b1) ? Red_level : 4'b0000 ;
-assign Green_i = (disp_ena == 1'b1) ? Green_level : 4'b0000 ;
-assign Blue_i = (disp_ena == 1'b1) ? Blue_level : 4'b0000 ;
+assign Red_i = (vga_chain_start.t.en == 1'b1) ? Red_level : 4'b0000 ;
+assign Green_i = (vga_chain_start.t.en == 1'b1) ? Green_level : 4'b0000 ;
+assign Blue_i = (vga_chain_start.t.en == 1'b1) ? Blue_level : 4'b0000 ;
 
 // outputs assigns
-assign pxl_x = Pxl_x_i;
-assign pxl_y = Pxl_y_i;
+assign pxl_x = vga_chain_start.t.pxl_x;
+assign pxl_y = vga_chain_start.t.pxl_y;
 assign Red = Red_i;
 assign Green = Green_i;
 assign Blue = Blue_i;
@@ -88,15 +80,15 @@ assign Blue = Blue_i;
 generate
 if (RGB_LAT == 0)
 begin
-	assign h_sync = h_sync_d;
-	assign v_sync = v_sync_d;
+	assign h_sync = vga_chain_start.t.hsync;
+	assign v_sync = vga_chain_start.t.vsync;
 end
 else
 begin
 	reg [RGB_LAT-1:0]h_dly;
 	reg [RGB_LAT-1:0]v_dly;
-	wire [RGB_LAT:0]tmp_h = {h_sync_d, h_dly};
-	wire [RGB_LAT:0]tmp_v = {v_sync_d, v_dly};
+	wire [RGB_LAT:0]tmp_h = {vga_chain_start.t.hsync, h_dly};
+	wire [RGB_LAT:0]tmp_v = {vga_chain_start.t.vsync, v_dly};
 	always @(posedge clk_25)
 	begin
 		h_dly <= tmp_h[RGB_LAT:1];
