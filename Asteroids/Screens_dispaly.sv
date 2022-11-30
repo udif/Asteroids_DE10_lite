076f25c5dda56391e021c840e05e2981cf8d3a95
Udi Finkelstein <github@udifink.com>
2022-11-30 20:39:05 +0200

    Fixed LCD screen stability problems.

--- Asteroids/Screens_dispaly.sv
+++ Asteroids/Screens_dispaly.sv
@@ -12,8 +12,6 @@ module Screens_dispaly # (
 	
 	input					clk_25,
 	input					clk_100,
-	output				h_sync,
-	output				v_sync,
 	vga.in		vga_chain_end,   // back from all display units
 	vga.out		vga_chain_start, // start chain with only sync & pixel x/y signals
 	vga.out		vga_out,         // now send to VGA connector
@@ -23,27 +21,31 @@ module Screens_dispaly # (
 	output				lcd_d_c,
 	output				lcd_rd
 	);
-	
+
 // VGA controller
  vga_controller VGA_interface (
 	.pixel_clk  (clk_25),
 	.reset_n    (1),
     .vga_gen    (vga_chain_start)
 );
-	
+
+vga vga_buf();
+always_ff @(posedge clk_25)
+	vga_buf.t <= vga_chain_end.t;
+
 // LCD controller
 lcd_ctrl LCD_interface(
 	.clk_50(0), // not used
 	.clk_25(clk_25),
 	.clk_100(clk_100),
 	.resetN(1),
-	.pxl_x(vga_chain_start.t.pxl_x),
-	.pxl_y(vga_chain_start.t.pxl_y),
+	.pxl_x(vga_buf.t.pxl_x),
+	.pxl_y(vga_buf.t.pxl_y),
 	.h_sync(0), // not used
 	.v_sync(0), // not used
-	.red_in(vga_out.t.red),
-	.green_in(vga_out.t.green),
-	.blue_in(vga_out.t.blue),
+	.red_in  (vga_buf.t.red),
+	.green_in(vga_buf.t.green),
+	.blue_in (vga_buf.t.blue),
 	.sw_0(1), // used for reset
 	.lcd_db(lcd_db),
 	.lcd_reset(lcd_reset),
@@ -61,8 +63,8 @@ assign vga_out.t.blue  = (vga_chain_start.t.en == 1'b1) ? vga_chain_end.t.blue  
 generate
 if (RGB_LAT == 0)
 begin
-	assign h_sync = vga_chain_start.t.hsync;
-	assign v_sync = vga_chain_start.t.vsync;
+	assign vga_out.t.hsync = vga_chain_start.t.hsync;
+	assign vga_out.t.vsync = vga_chain_start.t.vsync;
 end
 else
 begin
@@ -75,12 +77,9 @@ begin
 		h_dly <= tmp_h[RGB_LAT:1];
 		v_dly <= tmp_v[RGB_LAT:1];
 	end
-	assign h_sync = h_dly[0];
-	assign v_sync = v_dly[0];
+	assign vga_out.t.hsync = h_dly[0];
+	assign vga_out.t.vsync = v_dly[0];
 end
 endgenerate
-
-assign vga_out.t.hsync = h_sync;
-assign vga_out.t.vsync = v_sync;
 
 endmodule
