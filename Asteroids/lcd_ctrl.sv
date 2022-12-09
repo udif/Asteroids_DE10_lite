93bab92b21c041c4a85969db0b8c6bbcb430a7f0
Udi Finkelstein <github@udifink.com>
2022-12-09 08:59:54 +0200

    cleanup tabs and code reformatting, no rtl changes

--- Asteroids/lcd_ctrl.sv
+++ Asteroids/lcd_ctrl.sv
@@ -3,7 +3,7 @@
 // Designer: Avi Salmon,
 // 20/8/2022
 // ***********************************************
-// 
+//
 // This module Captures the signals going to the VGA screen and translates in
 // to the 480x800 LCD module
 //
@@ -20,7 +20,7 @@
 
 
 module lcd_ctrl (
-	input 				clk_50, 
+	input 				clk_50,
 	input 				clk_25,
 	input					clk_100,
 	input 				resetN,
@@ -32,15 +32,13 @@ module lcd_ctrl (
 	input		[3:0]		green_in,
 	input		[3:0]		blue_in,
 	input					sw_0,
-	
+
 	output	[7:0]		lcd_db,
 	output				lcd_reset,
 	output				lcd_wr,
 	output				lcd_d_c,
 	output				lcd_rd
 	);
-	
-
 
 reg	[25:0]	count;
 wire				count_start;
@@ -60,29 +58,24 @@ wire				cmd_lcd_d_c;
 wire				rgb_lcd_wr;
 wire				cmd_lcd_wr;
 
+assign lcd_db =
+	rgb_data_1 == 1 ? {red_in[3:0], 1'b0, green_in[3:1]} :
+	rgb_data_2 == 1 ? {green_in[0], 2'b0, blue_in[3:0], 1'b0} :
+	                  lcd_data;
 
-
-assign lcd_db = rgb_data_1 == 1? { red_in[3:0], 1'b0, green_in[3:1]}: 
-					 rgb_data_2 == 1? { green_in[0], 2'b0, blue_in[3:0], 1'b0}:
-					 lcd_data;
-					 
-assign lcd_rd = 1;
-assign lcd_wr = rgb_lcd_wr | cmd_lcd_wr;
+assign lcd_rd  = 1;
+assign lcd_wr  = cmd_lcd_wr  | rgb_lcd_wr;
 assign lcd_d_c = cmd_lcd_d_c | rgb_lcd_d_c;
-	
+
 // General counter
 
 always_ff @(posedge clk_25) begin
-	if (count_start == 1) 
-		begin
-			count <= 0;
-		end
-	else
-		begin
-			count <= count + 1'b1;
-		end
+	if (count_start == 1) begin
+		count <= 0;
+	end else begin
+		count <= count + 1'b1;
+	end
 end
-
 
 always_comb begin
 	last_line = pxl_x == 639;
@@ -90,7 +83,7 @@ always_comb begin
 	last_pixel = last_line & (pxl_y == 479);
 	first_pixel = (pxl_x == 0) & (pxl_y == 0);
 end
-	
+
 `define ONE_HOT
 `ifdef ONE_HOT
 `define P(x) (1 << (x))
@@ -100,7 +93,7 @@ end
 `define W(x) ($clog2(x))
 `endif
 
-typedef enum int unsigned { 
+typedef enum int unsigned {
 	HOLD 		= `P( 0),
 	CMD_1_d		= `P( 1),
 	CMD_1_u		= `P( 2),
@@ -136,295 +129,261 @@ typedef enum int unsigned {
 	DATA_e_2_u	= `P(28)
 } state_t;
 logic [`W(state_t.num())-1:0] state, next_state;
-	
-always_comb 
-	begin
-		next_state = HOLD;
-		cmd_lcd_wr = 1'b1;
-		count_start = 0;
-		add_init = 0;
-		add_next = 0;
-		cmd_lcd_d_c = 0;
-		lcd_reset = 1;
-		rgb_on = 0;
-		case(state)
-			
-			HOLD: begin
-						if (sw_0 == 1) begin
-							next_state = IDLE;
-							add_init = 1;
-							count_start = 1;
-							end
-						else begin
-							next_state = HOLD;
-							
-						end
-					end
-			
-			IDLE: begin
-						if (count != 20000000) begin
-							next_state = IDLE;
-							add_init = 1;
-							end
-						else begin
-							next_state = CMD_1_d;
-							count_start = 1;
-						end
-						if (count < 128) begin
-							lcd_reset = 0;
-						end
-					end
-			
-			CMD_1_d: begin
-							//lcd_d_c = 1;
-							next_state = CMD_1_u;
-							add_next = 1;
-							cmd_lcd_wr = 0;
-						end
-						
-			CMD_1_u: begin
-							//lcd_d_c = 1;
-							cmd_lcd_wr = 1;
-							
-							next_state = CMD_2_d;
-						end
-			
-			CMD_2_d: begin			
-							//lcd_d_c = 1;
-							next_state = CMD_2_u;
-							add_next = 1;
-							cmd_lcd_wr = 0;
-						end
-			
-			CMD_2_u: begin
-			
-							cmd_lcd_wr = 1;	
-							if (address != 11'h60E) begin
-								next_state = DATA_1_d;
-							end
-							else begin
-								next_state = DELAY_1;
-								
-								
-							end
-						 end
-						 
-			
-			DATA_1_d: begin
-							cmd_lcd_d_c = 1;
-							next_state = DATA_1_u;
-							add_next = 1;
-							cmd_lcd_wr = 0;
-						end
-						
-			DATA_1_u: begin
-							
-							cmd_lcd_d_c = 1;
-							cmd_lcd_wr = 1;
-							
-							next_state = DATA_2_d;
-						end
-			
-			DATA_2_d: begin
-							cmd_lcd_d_c = 1;
-							next_state = DATA_2_u;
-							add_next = 1;
-							cmd_lcd_wr = 0;
-						end
-			
-			DATA_2_u: begin
-							cmd_lcd_d_c = 1;
-							cmd_lcd_wr = 1;	
-							
-							next_state = CMD_1_d;
 
-						 end
-						 
-			
-			
-//			CMD_3_d: begin
-//							next_state = CMD_3_u;
-//							add_next = 1;
-//							cmd_lcd_wr = 0;
-//						 end
-						 
-//			CMD_3_u: begin
-//							cmd_lcd_wr = 1;
-//							
-//							next_state = CMD_4_d;
-//						 end
-//						 
-//			CMD_4_d: begin
-//							next_state = CMD_4_u;
-//							add_next = 1;
-//							cmd_lcd_wr = 0;
-//						 end
-//						 
-//			CMD_4_u: begin
-//							cmd_lcd_wr = 1;
-//							//add_next = 1;
-//							next_state = DELAY_1;
-//							count_start = 1;
-//						 end
-						 
-						 
-			
-			DELAY_1: begin
-							//lcd_d_c = 1;
-							if (count != 3000000) begin
-								next_state = DELAY_1;
-								cmd_lcd_d_c = 1;
-								end
-							else begin
-								next_state = CMD_5_d;
-								
-							end
-						end
-			CMD_5_d: begin
-							next_state = CMD_5_u;
-							add_next = 1;
-							cmd_lcd_wr = 0;
-						 end
-						 
-			CMD_5_u: begin
-							cmd_lcd_wr = 1;
-							
-							next_state = CMD_6_d;
-						 end
-						 
-			CMD_6_d: begin
-							next_state = CMD_6_u;
-							add_next = 1;
-							cmd_lcd_wr = 0;
-						 end
-						 
-			CMD_6_u: begin
-							cmd_lcd_wr = 1;
-							//add_next = 1;
-							next_state = DELAY_2;
-							count_start = 1;
-						 end
-			
-			DELAY_2: begin
-							cmd_lcd_d_c = 1;
-							if (count != 3000000) begin
-								next_state = DELAY_2;
-								end
-							else begin
-								next_state = CMD_e_1_d;
-								
-							end
-						end	
-						
-			///////
-			CMD_e_1_d: begin
-							//lcd_d_c = 1;
-							next_state = CMD_e_1_u;
-							add_next = 1;
-							cmd_lcd_wr = 0;
-						end
-						
-			CMD_e_1_u: begin
-							//lcd_d_c = 1;
-							cmd_lcd_wr = 1;
-							
-							next_state = CMD_e_2_d;
-						end
-			
-			CMD_e_2_d: begin			
-							//lcd_d_c = 1;
-							next_state = CMD_e_2_u;
-							add_next = 1;
-							cmd_lcd_wr = 0;
-						end
-			
-			CMD_e_2_u: begin
-			
-							cmd_lcd_wr = 1;	
-							if (address != 11'h636) begin
-								next_state = DATA_e_1_d;
-							end
-							else begin
-								next_state = RGB_wait;
-								
-								
-							end
-						 end
-						 
-			
-			DATA_e_1_d: begin
-							cmd_lcd_d_c = 1;
-							next_state = DATA_e_1_u;
-							add_next = 1;
-							cmd_lcd_wr = 0;
-						end
-						
-			DATA_e_1_u: begin
-							
-							cmd_lcd_d_c = 1;
-							cmd_lcd_wr = 1;
-							
-							next_state = DATA_e_2_d;
-						end
-			
-			DATA_e_2_d: begin
-							cmd_lcd_d_c = 1;
-							next_state = DATA_e_2_u;
-							add_next = 1;
-							cmd_lcd_wr = 0;
-						end
-			
-			DATA_e_2_u: begin
-							cmd_lcd_d_c = 1;
-							cmd_lcd_wr = 1;	
-							
-							next_state = CMD_e_1_d;
+always_comb begin
+	next_state = HOLD;
+	cmd_lcd_wr = 1'b1;
+	count_start = 0;
+	add_init = 0;
+	add_next = 0;
+	cmd_lcd_d_c = 0;
+	lcd_reset = 1;
+	rgb_on = 0;
+	case(state)
 
-						 end
-			
-			///////
-				
-			RGB_CMD_1_d: begin
-								next_state = RGB_CMD_1_u;
-								add_next = 1;
-								cmd_lcd_wr = 0;
-							end
-						
-			RGB_CMD_1_u: begin
-								cmd_lcd_wr = 1;
-								next_state = RGB_CMD_2_d;
-							end
-			
-			RGB_CMD_2_d: begin			
-								next_state = RGB_CMD_2_u;
-								cmd_lcd_wr = 0;
-								//add_next = 1;
-							end
-			
-			RGB_CMD_2_u: begin
-			
-							cmd_lcd_wr = 1;	
-							next_state = RGB_wait;
-						 end		
-			
-			
-			RGB_wait: begin
-							next_state = RGB_wait;
-							//count_start = 1;
-							rgb_on = 1;
-							cmd_lcd_wr = 0;
-						 end
-						 
-			default: 
-				begin
-					next_state = HOLD;
+		HOLD: begin
+			if (sw_0 == 1) begin
+				next_state = IDLE;
+				add_init = 1;
+				count_start = 1;
+			end else begin
+				next_state = HOLD;
+			end
+		end
+
+		IDLE: begin
+			if (count != 20000000) begin
+				next_state = IDLE;
+				add_init = 1;
+			end else begin
+				next_state = CMD_1_d;
+				count_start = 1;
+			end
+			if (count < 128) begin
+				lcd_reset = 0;
+			end
+		end
+
+		CMD_1_d: begin
+			//lcd_d_c = 1;
+			next_state = CMD_1_u;
+			add_next = 1;
+			cmd_lcd_wr = 0;
+		end
+
+		CMD_1_u: begin
+			//lcd_d_c = 1;
+			cmd_lcd_wr = 1;
+			next_state = CMD_2_d;
+		end
+
+		CMD_2_d: begin
+			//lcd_d_c = 1;
+			next_state = CMD_2_u;
+			add_next = 1;
+			cmd_lcd_wr = 0;
+		end
+
+		CMD_2_u: begin
+			cmd_lcd_wr = 1;
+			if (address != 11'h60E) begin
+				next_state = DATA_1_d;
+			end else begin
+				next_state = DELAY_1;
+			end
+		end
+
+		DATA_1_d: begin
+			cmd_lcd_d_c = 1;
+			next_state = DATA_1_u;
+			add_next = 1;
+			cmd_lcd_wr = 0;
+		end
+
+		DATA_1_u: begin
+			cmd_lcd_d_c = 1;
+			cmd_lcd_wr = 1;
+			next_state = DATA_2_d;
+		end
+
+		DATA_2_d: begin
+			cmd_lcd_d_c = 1;
+			next_state = DATA_2_u;
+			add_next = 1;
+			cmd_lcd_wr = 0;
+		end
+
+		DATA_2_u: begin
+			cmd_lcd_d_c = 1;
+			cmd_lcd_wr = 1;
+			next_state = CMD_1_d;
+		end
+
+//		CMD_3_d: begin
+//			next_state = CMD_3_u;
+//			add_next = 1;
+//			cmd_lcd_wr = 0;
+//		end
+
+//		CMD_3_u: begin
+//			cmd_lcd_wr = 1;
+//			next_state = CMD_4_d;
+//		end
+//
+//		CMD_4_d: begin
+//			next_state = CMD_4_u;
+//			add_next = 1;
+//			cmd_lcd_wr = 0;
+//		end
+
+//		CMD_4_u: begin
+//			cmd_lcd_wr = 1;
+//			//add_next = 1;
+//			next_state = DELAY_1;
+//			count_start = 1;
+//		end
+
+		DELAY_1: begin
+			//lcd_d_c = 1;
+			if (count != 3000000) begin
+				next_state = DELAY_1;
+				cmd_lcd_d_c = 1;
 				end
-		endcase
-	end
+			else begin
+				next_state = CMD_5_d;
+			end
+		end
+
+		CMD_5_d: begin
+			next_state = CMD_5_u;
+			add_next = 1;
+			cmd_lcd_wr = 0;
+		end
+
+		CMD_5_u: begin
+			cmd_lcd_wr = 1;
+			next_state = CMD_6_d;
+		end
+
+		CMD_6_d: begin
+			next_state = CMD_6_u;
+			add_next = 1;
+			cmd_lcd_wr = 0;
+		end
+
+		CMD_6_u: begin
+			cmd_lcd_wr = 1;
+			//add_next = 1;
+			next_state = DELAY_2;
+			count_start = 1;
+		end
+
+		DELAY_2: begin
+			cmd_lcd_d_c = 1;
+			if (count != 3000000) begin
+				next_state = DELAY_2;
+				end
+			else begin
+				next_state = CMD_e_1_d;
+			end
+		end
+
+		///////
+		CMD_e_1_d: begin
+			//lcd_d_c = 1;
+			next_state = CMD_e_1_u;
+			add_next = 1;
+			cmd_lcd_wr = 0;
+		end
+
+		CMD_e_1_u: begin
+			//lcd_d_c = 1;
+			cmd_lcd_wr = 1;
+			next_state = CMD_e_2_d;
+		end
+
+		CMD_e_2_d: begin
+			//lcd_d_c = 1;
+			next_state = CMD_e_2_u;
+			add_next = 1;
+			cmd_lcd_wr = 0;
+		end
+
+		CMD_e_2_u: begin
+			cmd_lcd_wr = 1;
+			if (address != 11'h636) begin
+				next_state = DATA_e_1_d;
+			end
+			else begin
+				next_state = RGB_wait;
+			end
+		end
+
+		DATA_e_1_d: begin
+			cmd_lcd_d_c = 1;
+			next_state = DATA_e_1_u;
+			add_next = 1;
+			cmd_lcd_wr = 0;
+		end
+
+		DATA_e_1_u: begin
+			cmd_lcd_d_c = 1;
+			cmd_lcd_wr = 1;
+			next_state = DATA_e_2_d;
+		end
+
+		DATA_e_2_d: begin
+			cmd_lcd_d_c = 1;
+			next_state = DATA_e_2_u;
+			add_next = 1;
+			cmd_lcd_wr = 0;
+		end
+
+		DATA_e_2_u: begin
+			cmd_lcd_d_c = 1;
+			cmd_lcd_wr = 1;
+			next_state = CMD_e_1_d;
+		end
+		///////
+		RGB_CMD_1_d: begin
+			next_state = RGB_CMD_1_u;
+			add_next = 1;
+			cmd_lcd_wr = 0;
+		end
+
+		RGB_CMD_1_u: begin
+			cmd_lcd_wr = 1;
+			next_state = RGB_CMD_2_d;
+		end
+
+		RGB_CMD_2_d: begin
+			next_state = RGB_CMD_2_u;
+			cmd_lcd_wr = 0;
+			//add_next = 1;
+		end
+
+		RGB_CMD_2_u: begin
+			cmd_lcd_wr = 1;
+			next_state = RGB_wait;
+		end
+
+		RGB_wait: begin
+			next_state = RGB_wait;
+			//count_start = 1;
+			rgb_on = 1;
+			cmd_lcd_wr = 0;
+		end
+
+		default:
+			begin
+				next_state = HOLD;
+			end
+	endcase
+end
 
 always_ff @(posedge clk_25) begin
-		state <= next_state;
-	end
+	state <= next_state;
+end
 
 // Memory with commands to the LCD.
 
@@ -433,9 +392,9 @@ lcd_cmd lcd_cmd_inst(
 	.clock(clk_25),
 	.q(lcd_data[7:0])
 	);
-	
+
 // address counter
-always_ff @(posedge clk_25) 
+always_ff @(posedge clk_25)
 	begin
 		if (add_init) begin
 			address <= 0;
@@ -444,11 +403,10 @@ always_ff @(posedge clk_25)
 		address <= address + 1'b1;
 		end
 	end
-	
 
 // RGB state machine
 
-typedef enum int unsigned { 
+typedef enum int unsigned {
 	RGB_IDLE            = `P( 0),
 	RGB_WAIT_START      = `P( 1),
 	RGB_DATA_1_d        = `P( 2),
@@ -461,127 +419,104 @@ typedef enum int unsigned {
 } rgb_states_t;
 logic [`W(rgb_states_t.num())-1:0]rgb_state, rgb_next_state;
 
-always_comb 
-	begin
-		rgb_next_state = RGB_IDLE;
-		rgb_data_1 = 0;
-		rgb_data_2 = 0;
-		rgb_lcd_wr = 1;
-		rgb_lcd_d_c = 0;
-		case(rgb_state)
+always_comb begin
+	rgb_next_state = RGB_IDLE;
+	rgb_data_1 = 0;
+	rgb_data_2 = 0;
+	rgb_lcd_wr = 1;
+	rgb_lcd_d_c = 0;
+	case(rgb_state)
+		RGB_IDLE: begin
+			if (rgb_on == 1) begin
+				rgb_next_state = RGB_WAIT_START;
+				rgb_lcd_wr = 1;
+			end else begin
+				rgb_next_state = RGB_IDLE;
+				rgb_lcd_wr = 0;
+			end
+		end
 
-			
-			RGB_IDLE: begin
-							
-							if (rgb_on == 1) begin
-								rgb_next_state = RGB_WAIT_START;
-								rgb_lcd_wr = 1;
-							end
-							else begin
-									rgb_next_state = RGB_IDLE;
-									rgb_lcd_wr = 0;
-								end
-							end
-					
-			RGB_WAIT_START: begin
-									
-									if (first_pixel) begin
-										rgb_lcd_d_c = 1;
-										rgb_next_state = RGB_DATA_1_u;
-										rgb_data_1 = 1;
-										rgb_lcd_wr = 0;
-									end
-									else begin
-										rgb_next_state = RGB_WAIT_START;
-										rgb_lcd_wr = 1;
-									end
-								end
-								
-			RGB_DATA_1_d: begin
-							
-							rgb_lcd_d_c = 1;
-							rgb_next_state = RGB_DATA_1_u;
-							rgb_data_1 = 1;
-							rgb_lcd_wr = 0;
-							//add_next = 1;
-							
-						end
-						
-			RGB_DATA_1_u: begin
-							
-							rgb_lcd_d_c = 1;
-							rgb_lcd_wr = 1;
-							rgb_data_1 = 1;
-							rgb_next_state = RGB_DATA_2_d;
-						end
-			
-			RGB_DATA_2_d: begin
-							rgb_lcd_d_c = 1;
-							rgb_next_state = RGB_DATA_2_u;
-							rgb_data_2 = 1;
-							rgb_lcd_wr = 0;
-							//add_next = 1;
-						end
-			
-			RGB_DATA_2_u: begin
-								rgb_lcd_d_c = 1;
-								rgb_lcd_wr = 1;	
-								rgb_data_2 = 1;
-								
-								
-							
-								if (last_pixel) begin
-									rgb_next_state = RGB_WAIT_NEXT_FRAME;
-								end
-								else if (last_line == 1) begin
-									rgb_next_state = RGB_WAIT_NEXT_LINE;
-								end
-								else begin
-									rgb_next_state = RGB_DATA_1_d;
-								end
-								
-								
-							end
-	
-			RGB_WAIT_NEXT_LINE: begin
-										rgb_lcd_d_c = 1;
-										rgb_data_1 = 1;
-										if (first_line == 1) begin
-												rgb_next_state = RGB_DATA_1_u;
-												rgb_lcd_wr = 0;
-											end
-										else begin
-												rgb_next_state = RGB_WAIT_NEXT_LINE;
-											end
-										end
-										
-			RGB_WAIT_NEXT_FRAME: begin
-										rgb_lcd_d_c = 1;
-										rgb_data_1 = 1;
-										if (first_pixel) begin
-												rgb_next_state = RGB_DATA_1_u;
-												rgb_lcd_wr = 0;
-											end
-										else begin
-												rgb_next_state = RGB_WAIT_NEXT_FRAME;
-											end
-										end
-										
-			STUCK: rgb_next_state = STUCK;
-			
-			default: 
-				begin
-					rgb_next_state = RGB_IDLE;
-				end
-				
-		endcase
-	end
-	
+		RGB_WAIT_START: begin
+			if (first_pixel) begin
+				rgb_lcd_d_c = 1;
+				rgb_next_state = RGB_DATA_1_u;
+				rgb_data_1 = 1;
+				rgb_lcd_wr = 0;
+			end else begin
+				rgb_next_state = RGB_WAIT_START;
+				rgb_lcd_wr = 1;
+			end
+		end
+
+		RGB_DATA_1_d: begin
+			rgb_lcd_d_c = 1;
+			rgb_next_state = RGB_DATA_1_u;
+			rgb_data_1 = 1;
+			rgb_lcd_wr = 0;
+			//add_next = 1;
+		end
+
+		RGB_DATA_1_u: begin
+			rgb_lcd_d_c = 1;
+			rgb_lcd_wr = 1;
+			rgb_data_1 = 1;
+			rgb_next_state = RGB_DATA_2_d;
+		end
+
+		RGB_DATA_2_d: begin
+			rgb_lcd_d_c = 1;
+			rgb_next_state = RGB_DATA_2_u;
+			rgb_data_2 = 1;
+			rgb_lcd_wr = 0;
+			//add_next = 1;
+		end
+
+		RGB_DATA_2_u: begin
+			rgb_lcd_d_c = 1;
+			rgb_lcd_wr = 1;
+			rgb_data_2 = 1;
+			if (last_pixel) begin
+				rgb_next_state = RGB_WAIT_NEXT_FRAME;
+			end else if (last_line == 1) begin
+				rgb_next_state = RGB_WAIT_NEXT_LINE;
+			end else begin
+				rgb_next_state = RGB_DATA_1_d;
+			end
+		end
+
+		RGB_WAIT_NEXT_LINE: begin
+			rgb_lcd_d_c = 1;
+			rgb_data_1 = 1;
+			if (first_line == 1) begin
+				rgb_next_state = RGB_DATA_1_u;
+				rgb_lcd_wr = 0;
+			end else begin
+				rgb_next_state = RGB_WAIT_NEXT_LINE;
+			end
+		end
+
+		RGB_WAIT_NEXT_FRAME: begin
+			rgb_lcd_d_c = 1;
+			rgb_data_1 = 1;
+			if (first_pixel) begin
+				rgb_next_state = RGB_DATA_1_u;
+				rgb_lcd_wr = 0;
+			end else begin
+				rgb_next_state = RGB_WAIT_NEXT_FRAME;
+			end
+		end
+
+		STUCK:
+			rgb_next_state = STUCK;
+
+		default:  begin
+			rgb_next_state = RGB_IDLE;
+		end
+
+	endcase
+end
+
 always_ff @(posedge clk_100) begin
-		rgb_state <= rgb_next_state;
-	end	
-
-
-										
-										
+	rgb_state <= rgb_next_state;
+end
 endmodule
